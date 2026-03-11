782a61f839a9598613538a132ba38809034627c5
Tapaswini <tapaswini8024@gmail.com>
2026-03-11 13:36:48 +0530

    Coded UC3: Display Hello with Command-Line Argument or Default Message

--- Oopsbanner2.java
+++ Oopsbanner2.java
@@ -1,12 +1,27 @@
-public class Oopsbanner2{
-    public static void main(String args[]){
-        System.out.println(" ***** "+" "+" ***** "+" "+"*****  "+" "+" **** ");
-        System.out.println("**   **"+" "+"**   **"+" "+"**   **"+" "+"**    ");
-        System.out.println("**   **"+" "+"**   **"+" "+"**   **"+" "+"**    ");
-        System.out.println("**   **"+" "+"**   **"+" "+"*****  "+" "+" **** ");
-        System.out.println("**   **"+" "+"**   **"+" "+"**     "+" "+"    **");
-        System.out.println("**   **"+" "+"**   **"+" "+"**     "+" "+"    **");
-        System.out.println(" ***** "+" "+" ***** "+" "+"**     "+" "+" **** ");
+public class UC2_OOPS_Banner {
+
+    public static void main(String[] args) {
+
+        // Line 1
+        System.out.println(" *****    *****    ******   ***** ");
+
+        // Line 2
+        System.out.println("*     *  *     *  *     *  *     *");
+
+        // Line 3
+        System.out.println("*     *  *     *  *     *  *     *");
+
+        // Line 4
+        System.out.println("*     *  *     *  ******   ***** ");
+
+        // Line 5
+        System.out.println("*     *  *     *  *        *   * ");
+
+        // Line 6
+        System.out.println("*     *  *     *  *        *    *");
+
+        // Line 7
+        System.out.println(" *****    *****   *        *     *");
+
     }
 }
-
