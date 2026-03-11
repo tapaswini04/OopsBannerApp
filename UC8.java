24208f8c88d140767681ea9c07b6cf57ae5a300f
Tapaswini <tapaswini8024@gmail.com>
2026-03-11 19:18:25 +0530

    Implemented UC8: Use HashMap for character patterns and render banner via function

---
+++ UC8.java
@@ -0,0 +1,72 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class UC8 {
+
+    // Method to create character patterns using HashMap
+    public static Map<Character, String[]> createPatternMap() {
+
+        Map<Character, String[]> patternMap = new HashMap<>();
+
+        patternMap.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        patternMap.put('P', new String[]{
+                "*****  ",
+                "*    * ",
+                "*    * ",
+                "*****  ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        patternMap.put('S', new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        });
+
+        return patternMap;
+    }
+
+    // Method to display the banner
+    public static void displayBanner(String word, Map<Character, String[]> patternMap) {
+
+        for (int row = 0; row < 7; row++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char c : word.toCharArray()) {
+
+                String[] pattern = patternMap.get(c);
+
+                if (pattern != null) {
+                    line.append(pattern[row]).append("  ");
+                }
+            }
+
+            System.out.println(line);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Map<Character, String[]> patternMap = createPatternMap();
+
+        String word = "OOPS";
+
+        displayBanner(word, patternMap);
+    }
+}
